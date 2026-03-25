d71a42a007c129c2575088ba3b97bcb3a9a84a92
whypikmi <ttelatnikova1@yandex.ru>
2026-03-25 14:36:06 +0300

    Step 7: Setup NavHost and complete navigation

--- app/src/main/java/com/zlamanyuk/androidstudio_lab15_16/MainActivity.kt
+++ app/src/main/java/com/zlamanyuk/androidstudio_lab15_16/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.zlamanyuk.androidstudio_lab15_16.navigation.StudentPlannerNavHost
 import com.zlamanyuk.androidstudio_lab15_16.ui.theme.AndroidStudio_Lab1516Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidStudio_Lab1516Theme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    StudentPlannerNavHost(
+                        navController = navController,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidStudio_Lab1516Theme {
-        Greeting("Android")
     }
 }
